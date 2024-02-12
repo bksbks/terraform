7ab5378849144b3c29d41b25cc8fa9b5d0c4e7bb
barkath <barakathullass@gmail.com>
2024-02-12 16:20:33 +0530

    Updated files

--- main.tf
+++ main.tf
@@ -2,12 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-
-}
-output "public_ip" {
-  description = "public_ip of instance"
-  value       = aws_instance.my_instance.public_ip
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-053b0d53c279acc90" # replace this
+  instance_type_value = "t2.micro"
+  #subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "ami_value" {
-    description = "ami id of instance"
-}
-
-variable "instance_type_value" {
-    description = "type of instance"
-}    
